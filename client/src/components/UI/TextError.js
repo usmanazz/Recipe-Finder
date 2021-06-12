046de2ac4a1eb13f704d218c18319cb57c261af8
Usman Naz <unnaz1@MacBook-Pro.local>
2021-06-11 18:57:51 -0700

    formik second implementation

---
+++ client/src/components/UI/TextError.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TextError = (props) => {
+  return <div className="error">{props.children}</div>;
+};
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,45 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import "./Signup.css";
-import { Button } from "../../components/UI/Button";
 import { Link } from "react-router-dom";
 
 export const Signup = () => {
-  const handleSubmit = () => {
-    console.log("create account");
+  const [renderError, setRenderError] = useState(false);
+
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
   };
+
+  const onSubmit = (values) => {
+    console.log("Form data ", values);
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email format").required("Required"),
+    password: Yup.string()
+      .required("Required")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        "Must contain at least 8 characters, 1 letter and 1 number"
+      ),
+  });
 
   return (
     <div className="Signup">
       <div className="signup-box">
-        <h3 className="signup-title">CREATE AN ACCOUNT</h3>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => {
+            console.log("Formik props", formik);
+            const { name, email, password } = formik.values;
+            return (
+              <div>
+                <h3 className="signup-title">CREATE AN ACCOUNT</h3>
 
-        <form className="signup-form">
-          <div className="signup-field">
-            <label htmlFor="name">NAME</label>
-            <input type="text" id="name" name="name" placeholder="name" />
-          </div>
+                {console.log(renderError)}
+                {renderError ? (
+                  <div className="main-error-message error">
+                    Please fill out all of the fields
+                  </div>
+                ) : null}
 
-          <div className="signup-field">
-            <label htmlFor="email">EMAIL</label>
-            <input type="email" id="email" name="email" placeholder="email" />
-          </div>
+                <Form className="signup-form">
+                  <div className="signup-field">
+                    <label htmlFor="name">NAME</label>
+                    <Field type="text" id="name" name="name" />
+                    <ErrorMessage name="name">
+                      {(errMessage) => (
+                        <div className="error">{errMessage}</div>
+                      )}
+                    </ErrorMessage>
+                  </div>
 
-          <div className="signup-field">
-            <label htmlFor="password">PASSWORD</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="password"
-            />
-          </div>
-          <Button
-            handleClick={handleSubmit}
-            label="CREATE ACCOUNT"
-            type="create-account-btn"
-          />
-        </form>
+                  <div className="signup-field">
+                    <label htmlFor="email">EMAIL</label>
+                    <Field type="email" id="email" name="email" />
+                    <ErrorMessage name="email">
+                      {(errMessage) => (
+                        <div className="error">{errMessage}</div>
+                      )}
+                    </ErrorMessage>
+                  </div>
+
+                  <div className="signup-field">
+                    <label htmlFor="password">PASSWORD</label>
+                    <Field type="password" id="password" name="password" />
+                    <ErrorMessage name="password">
+                      {(errMessage) => (
+                        <div className="error">{errMessage}</div>
+                      )}
+                    </ErrorMessage>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="button create-account-btn"
+                    onClick={() => {
+                      !formik.isValid || !name || !email || !password
+                        ? setRenderError(true)
+                        : setRenderError(false);
+                    }}
+                  >
+                    CREATE ACCOUNT
+                  </button>
+                </Form>
+              </div>
+            );
+          }}
+        </Formik>
 
         <div className="login-redirect">
           <p>
